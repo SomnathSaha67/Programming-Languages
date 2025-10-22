a7d518b05ce993c17a651271a9171b41a7eb2b8b
SomnathSaha67 <somnath.saha.u2024@iem.edu.in>
2025-10-22 06:48:28 +0000

    Program swapping two numbers

---
+++ swap.py
@@ -0,0 +1,7 @@
+a=float(input("Enter first number: "))
+b=float(input("Enter second number: "))
+print(f"Values before swapping: a={a}, b={b}")
+c=a
+a=b
+b=c
+print(f"Values after swapping: a={a}, b={b}")
